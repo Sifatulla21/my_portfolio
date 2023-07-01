c64b8344a324b26917101888514d06f21e9cbd37
Sifatulla21 <sifat2109@gmail.com>
2023-07-01 11:44:11 +0600

    setup

---
+++ src/Component/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <input type="file" className="mt-48 file-input file-input-bordered file-input-primary w-full max-w-xs" />
+        </div>
+    );
+};
+
+export default Home;
